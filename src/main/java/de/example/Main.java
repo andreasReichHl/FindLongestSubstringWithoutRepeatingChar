22b2cd357ae897866ad8998de9ce8936ac4193fb
andreashl09 <a9279352@gmail.com>
2024-07-26 10:01:33 +0200

    add new project

---
+++ src/main/java/de/example/Main.java
@@ -0,0 +1,17 @@
+package de.example;
+
+public class Main {
+    public static void main(String[] args){
+
+        String s = "abcabcbb";
+        String s1 = "bbbbb";
+        String s3 = "pwwkew";
+    }
+
+    public static void findLongest(String text){
+        while(text.length()>1){
+
+
+        }
+    }
+}
